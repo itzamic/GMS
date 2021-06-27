28cc03688623d327b467d7d0203430659cbb712b
mike.zounaropoulos <mike.zounaropoulos@foodtecsolutions.com>
2021-06-27 08:33:07 -0400

    (feat) Add JPA and Postgres Dependencies
    - add application gradle plugin
    - set mainClass to GmsApplication
    - add spring actuator, data-jpa, lombok, devtools, rest-docs, configuration-processor and postgresql dependencies
    
    Closes #2

--- src/test/java/dev/itzamic/gms/GmsApplicationTests.java
+++ src/test/java/dev/itzamic/gms/GmsApplicationTests.java
@@ -7,9 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GmsApplicationTests {
 
-	@Test
-	void contextLoads() {
-		Assertions.assertTrue(true);
-	}
-
+  @Test
+  void contextLoads() {
+    Assertions.assertTrue(true);
+  }
 }
